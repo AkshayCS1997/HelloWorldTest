3006f5af7026a17ec2025a9d25d7efcb24b25f6c
AKSHAY C S <akshay.cs@ascendion.com>
2026-05-21 14:45:24 +0530

    Add files via upload
    1st commit

---
+++ HellowWorld/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HellowWorld
+{
+
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            Label lbl = new Label();
+            lbl.Text = "Hello World";
+            lbl.AutoSize = true;
+            lbl.Font = new System.Drawing.Font("Arial", 14);
+            lbl.Location = new System.Drawing.Point(50, 50);
+
+            this.Controls.Add(lbl);
+        }
+    }
+}
+
+}
